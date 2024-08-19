756b6de9e9a7939aeb2e733c1e1ed23ad5953878
CodiHaus <contact@codihaus.com>
2024-08-19 15:26:44 +0700

    fix: sidebar

--- app/components/layout/view/index.vue
+++ app/components/layout/view/index.vue
@@ -7,13 +7,17 @@
         </layout-header>
         <n-layout has-sider class="content-box">
             <layout-sidebar />
-            <slot name="sub-navigation"></slot>
+            <div v-if="$slots?.['sub-navigation']" class="w-62 h-full py-6 absolute z-99 top-0 bg-dark-05 lg:bg-transparent" :class="sidebarCollapsed ? 'top-0' : ''">
+                <div class="h-full bg-dark-05 rounded-tl-xl rounded-bl-xl">
+                    <slot name="sub-navigation"></slot>
+                </div>
+            </div>
             <n-layout-content :native-scrollbar="false" ref="contentRef" content-class="lg:py-6">
-                <div class="rounded-xl bg-hex-1A1D1E min-h-[calc(100dvh-121px)]">
+                <div class="content-wrapper rounded-xl bg-hex-1A1D1E">
                     <slot />
                 </div>
             </n-layout-content>
-            <div class="w-14">
+            <div :class="$slots?.['right-sidebar'] ? 'w-14' : 'w-4'">
                 <slot name="right-sidebar"></slot>
             </div>
         </n-layout>
@@ -22,20 +26,24 @@
 
 <script setup lang="ts">
 const contentRef = useState('chatboxRef', () => null)
+const sidebarCollapsed = useState('sidebarCollapsed')
 </script>
 
 <style lang="scss">
 .content-box {
     .n-layout-scroll-container {
         overflow: hidden;
-        height: 100vh;
-        height: 100dvh;
         display: flex;
         flex-direction: column;
-    }
-    .n-layout-content {
         height: calc(100vh - 73px);
         height: calc(100dvh - 73px);
     }
+    .n-layout-content {
+    }
+}
+
+.content-wrapper {
+    min-height: calc(100vh - 121px);
+    min-height: calc(100dvh - 121px);
 }
 </style>
