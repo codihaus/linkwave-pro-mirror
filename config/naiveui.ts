1c174ea0ec3bb35ccca8955393871709c9054bdf
CodiHaus <contact@codihaus.com>
2024-08-20 15:15:27 +0700

    chore: update config

--- config/naiveui.ts
+++ config/naiveui.ts
@@ -2,5 +2,5 @@ import type { ModuleOptions } from "@bg-dev/nuxt-naiveui";
 
 export const naiveui: Partial<ModuleOptions> = {
     colorModePreference: "light",
-    iconDownload: true,
+    iconDownload: false,
 };
