35ceb40ae518721b6e861a6cd952ab5ad9e43b7f
CodiHaus <contact@codihaus.com>
2024-08-21 17:19:06 +0700

    feat: project cost estimator detail #17

---
+++ app/views/project/cost-estimator/detail.vue
@@ -0,0 +1,204 @@
+<template>
+    <layout-view>
+        <template #header-right>
+            <n-button type="primary" icon class="px-2" style="--n-height: 40px;" @click="navigateTo('/projects/+')">
+                <i class="i-custom-plus text-2xl leading-0"></i>
+            </n-button>
+        </template>
+        <!-- <template #right-sidebar>
+            <lazy-right-sidebar />
+        </template>
+        <template #sub-navigation>
+            <sub-navigation />
+        </template> -->
+        <div class="">
+            <div class="flex items-center gap-3 py-3 px-4 lg:px-8 bg-dark-05">
+                <nuxt-link :to="goBack()">
+                    <i class="i-custom-arrow-narrow-left text-2xl leading-0 text-neutral-01"></i>
+                </nuxt-link>
+                <div class="font-semibold text-neutral-01">Estimation</div>
+                <div class="ml-auto">GRAND TOTAL: $141.147</div>
+            </div>
+            <div class="grid gap-5 lg:grid-cols-4 p-5 bg-hex-191B1B">
+                <div class="p-3 rounded border border-neutral-01 border-opacity-10 bg-neutral-07">
+                    <div class="text-white mb-3">PROPERTY OWNER</div>
+                    <div class="text-neutral-03 text-opacity-75 leading-loose">
+                        <div>Hong and Kim Pham</div>
+                        <div>(323) 574-1508</div>
+                        <div>hppzz@yahoo.com</div>
+                    </div>
+                </div>
+                <div class="p-3 rounded border border-neutral-01 border-opacity-10 bg-neutral-07">
+                    <div class="text-white mb-3">SCOPE OFWORK</div>
+                    <div class="text-neutral-03 text-opacity-75 leading-loose">
+                        <div>PROPOSED ONE STORY TYPE V DETACH ADU 496 SQ FT</div>
+                        <div>Tel: (423) 369-4717</div>
+                        <div>Email: Johndoe@gmail.com</div>
+                        <div>Add: 14391 Starsia St Westminster CA 92683</div>
+                    </div>
+                </div>
+                <div class="p-3 rounded border border-neutral-01 border-opacity-10 bg-neutral-07">
+                    <div class="text-white mb-3">REGIONAL</div>
+                    <div class="text-neutral-03 text-opacity-75 leading-loose">
+                        <div>1740 Gillette Crescent, South</div>
+                        <div>South Pasadena, CA 91030</div>
+                    </div>
+                </div>
+                <div class="p-3 rounded border border-neutral-01 border-opacity-10 bg-neutral-07">
+                    <div class="text-white mb-3">NAME OF FILE</div>
+                    <div class="text-neutral-03 text-opacity-75 leading-loose">
+                        <div>Remodeling Main house+Additional Room 287 Sqft.</div>
+                    </div>
+                </div>
+            </div>
+            <div class="">
+                <n-table
+                    :bordered="false"
+                    :single-line="false"
+                    style="
+                        --n-th-color: #14E3AE;
+                        --n-td-color: transparent;
+                        --n-border-color: #2E3133;
+                        --n-th-text-color: #100F0F;
+                        --n-td-text-color: #F0F5FB;
+                        --n-td-color-hover: #444;
+                        --n-td-padding: 16px;
+                    "
+                >
+                    <thead>
+                        <tr>
+                            <th v-for="th in columns">{{ th.title }}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Chuẩn Bị</td>
+                            <td>Đào đất làm đường ống nước và foundation</td>
+                            <td>Gói</td>
+                            <td>1</td>
+                        </tr>
+                    </tbody>
+                </n-table>
+                <!-- <n-data-table
+                    remote
+                    ref="table"
+                    :columns="columns"
+                    :data="data"
+                    :bordered="false"
+                    style="
+                        --n-th-color: transparent;
+                        --n-td-color: transparent;
+                        --n-border-color: #2E3133;
+                        --n-th-text-color: #E8ECEF;
+                        --n-td-text-color: #F0F5FB;
+                        --n-td-color-hover: #444;
+                        --n-td-padding: 16px;
+                    "
+                /> -->
+                    <!-- :loading="pending"
+                    :row-key="rowKey" -->
+            </div>
+        </div>
+
+    </layout-view>
+</template>
+<script setup lang="ts">
+import { readItems } from '@directus/sdk';
+import LazyRightSidebar from '../components/right-sidebar.vue'
+import SubNavigation from '../components/sub-navigation.vue'
+import { NTag } from 'naive-ui';
+import millify from 'millify'
+
+definePageMeta({
+	auth: true,
+    sidebarCollapsed: true,
+    hideSidebar: true
+})
+
+const route = useRoute()
+
+function goBack() {
+    return {
+        name: 'project-cost-estimator',
+        params: {
+            id: route.params?.id
+        }
+    }
+}
+
+const { data: project } = await useProject()
+
+const columns = [
+    {
+        title: 'No',
+        key: 'no',
+        // width: '30%',
+    },
+    {
+        title: 'Items group',
+        key: 'cost',
+    },
+    {
+        title: 'Description',
+        key: 'file.created_on'
+    },
+    {
+        title: 'Unit',
+        key: 'file.filesize',
+    },
+    {
+        title: 'Quantity',
+        key: 'status',
+    },
+    {
+        title: 'Unit cost ($)',
+        key: 'status',
+    },
+    {
+        title: 'Total($)',
+        key: 'status',
+    },
+    {
+        title: 'Note',
+        key: 'status',
+    },
+]
+
+function rowProps(row) {
+    return {
+        style: 'cursor: pointer;',
+        onClick: () => {
+            navigateTo({
+                name: 'project-cost-estimator-detail',
+                params: {
+                    id: route.params?.id,
+                    file_id: row?.id
+                }
+            })
+        }
+    }
+}
+
+const page = ref(1)
+const limit = ref(20)
+const api = useNAD()
+
+const { data, pending, refresh } = await useAsyncData(
+    () => api.request(readItems('estimator_group', {
+        fields: [ 'id', 'status', 'name', 'description' ],
+        limit: -1
+    })),
+    {
+        transform: (response) => {
+            console.log('response', response)
+            return response?.items
+        },
+    }
+)
+
+</script>
+
+<style lang="scss">
+
+</style>
